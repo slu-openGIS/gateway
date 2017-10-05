2729c35ee480ee6178cfc07b3985419ce06bbe0b
Christopher Prener, PhD <prenercg@slu.edu>
2017-10-05 11:02:53 -0500

    update numeric test so that it is consistent with structure of str test

--- tests/testthat/test_nhood_num.R
+++ tests/testthat/test_nhood_num.R
@@ -5,12 +5,13 @@ suppressPackageStartupMessages(library(dplyr))
 test_data <- data.frame(
     id = c(1, 2, 3, 4, 5),
     nhoodStr = c("Patch", "Bevo", "Bevo Mill", "Lindenwood Park", "Carondelet"),
+    nhoodResult = c(2, 5, 5, 9, 1),
     stringsAsFactors = FALSE
     )
 
 test_that("neighborhood numeric correctly identifies neighborhood", {
     result <- nhood_num(test_data$nhoodStr)
 
-  expect_equal(result, c(2, 5, 5, 9, 1))
+  expect_equal(result, test_data$nhoodResult)
 
 })
