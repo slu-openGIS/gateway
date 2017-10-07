4c5ae8a1fdc25b6aa603154bafdcfa1e168caed4
Christopher Prener <chris.prener@slu.edu>
2017-10-07 07:12:33 -0500

    add standardized streets documentation

---
+++ R/stdStreets.R
@@ -0,0 +1,15 @@
+#' Standardized Street Names
+#'
+#' A data set containing standardized street names and possible alternatives.
+#'
+#' @docType data
+#'
+#' @usage data(stdStreets)
+#'
+#' @format A data frame with 106 rows and 7 variables:
+#' \describe{
+#'   \item{dc_correct}{standardized street name}
+#'   \item{dc_incorrect}{alternative street name}
+#'   }
+#'
+"stdStreets"
