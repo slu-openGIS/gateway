31788218fc5168ca5e39ae1aab3d8db875c40977
Branson Fox <32143148+bransonf@users.noreply.github.com>
2018-10-28 02:42:51 -0500

    Update gw_get_repo.R

--- R/gw_get_repo.R
+++ R/gw_get_repo.R
@@ -54,7 +54,7 @@ gw_get_repo <- function(repo) {
   }
   else if (repo == "STL_BOUNDARY_Tracts") {
     url <- "https://github.com/slu-openGIS/STL_BOUNDARY_Tracts/archive/master.zip"
-    path <- "/STL_BOUNDARY_Tracts/Shapefile/STL_BOUNDART_Tracts.shp"
+    path <- "/STL_BOUNDARY_Tracts/Shapefile/STL_BOUNDARY_Tracts.shp"
 
   tmpdir <- tempdir()
   utils::download.file(url, paste0(tmpdir,"master.zip"))
