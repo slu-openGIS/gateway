441eb446a538d733604952a7ffb5cacf015c6c66
Christopher Prener <chris.prener@slu.edu>
2019-03-13 16:43:55 -0500

    save additional example file

--- inst/extdata/manaul_test_geocoder.R
+++ inst/extdata/manaul_test_geocoder.R
@@ -6,21 +6,24 @@ library(gateway)    # local geocoder functions
 library(postmastr)  # parse and standardize street addresses
 
 # load sample data from gateway
-sushi <- sushi
+sushi <- sushi2
 
 # clean and parse sample data
 sushi %>%
-  filter(name != "Drunken Fish - Ballpark Village") %>%
-  pm_parse(input = "short", address = "address") -> sushi_clean
+  dplyr::filter(name != "Drunken Fish - Ballpark Village") %>%
+  postmastr::pm_parse(input = "short", address = "address") %>%
+  dplyr::select(name, pm.address, visit) %>%
+  dplyr::rename(address = pm.address) -> sushi_clean
 
 # build local geocoder
 geocoder <- gw_build_geocoder(return = c("id", "coords"), class = "tibble", include_units = FALSE)
 
 # build sf version of local geocoder
-x <- gw_build_geocoder(class = "sf")
+geocoder <- gw_build_geocoder(class = "sf")
 
 # geocode
-y <- gw_geocode(sushi_clean, type = "local", class = "sf", address = "pm.address", geocoder = x)
+sushi_sf <- gw_geocode(sushi_clean, type = "local", class = "sf",
+                       address = "address", geocoder = geocoder)
 
 library(leaflet)
 
