05c95fe1f5d633903f5652d0be970c895b0a177d
Christopher Prener <chris.prener@slu.edu>
2019-03-13 14:10:34 -0500

    reformat standard street data

---
+++ R/stl_std_streets.R
@@ -0,0 +1,18 @@
+#' Standardized Street Names
+#'
+#' @description A data set containing standardized street names and possible alternatives.
+#'     This can be used as the basis for a dictionary appendix for the \pkg{postmastr}
+#'     package.
+#'
+#' @docType data
+#'
+#' @format A data frame with 12 rows and 2 variables:
+#' \describe{
+#'   \item{correct}{standardized street name}
+#'   \item{incorrect}{alternative street name}
+#'   }
+#'
+#' @examples
+#' head(stl_std_streets)
+#'
+"stl_std_streets"
--- inst/extdata/stdStreets.R
+++ inst/extdata/stdStreets.R
@@ -1,43 +1,16 @@
-dataCorr1 <- data.frame(
-  dc_correct = c("1st", "2nd", "4th", "6th", "7th", "8th", "9th",
-                 "10th", "11th", "13th", "14th", "15th",
-                 "16th", "17th","18th", "19th", "20th",
-                 "21st", "21st", "22nd", "22nd",
-                 "23rd", "23rd", "25th", "25th"),
-  dc_incorrect = c("First", "Second", "Fourth", "Sixth", "Seventh", "Eighth", "Ninth",
-                   "Tenth", "Eleventh", "Thirteenth", "Fourteenth", "Fifteenth",
-                   "Sixteenth", "Seventeenth", "Eighteenth", "Nineteenth", "Twentieth",
-                   "Twenty-First", "Twenthy First", "Twenty-Second", "Twenty Second",
-                   "Twenty-Third", "Twenty Third", "Twenty-Fifth", "Twenty Fifth"),
-  stringsAsFactors = FALSE
-)
 
-dataCorr2 <- data.frame(
-  dc_correct = c("1st", "2nd", "4th", "6th", "7th", "8th", "9th",
-                 "10th", "11th", "13th", "14th", "15th",
-                 "16th", "17th","18th", "19th", "20th",
-                 "21st", "22nd", "23rd", "25th"),
-  dc_incorrect = c("1St", "2Nd", "4Th", "6Th", "7Th", "8Th", "9Th",
-                   "10Th", "11Th", "13Th", "14Th", "15Th",
-                   "16Th", "17Th","18Th", "19Th", "20Th",
-                   "21St", "22Nd", "23Rd", "25Th"),
-  stringsAsFactors = FALSE
-)
 
-dataCorr3 <- data.frame(
-  dc_correct = c("Kingshighway", "Dr Martin Luther King", "Dr Martin Luther King", "Redd Foxx",
+stl_std_streets <- dplyr::tibble(
+  correct = c("Kingshighway", "Dr Martin Luther King", "Dr Martin Luther King", "Redd Foxx",
                  "Dr Samuel T Shepard", "Dr Samuel T Shepard", "Dr Samuel T Shepard", "Mount Pleasant",
                  "Morgan Ford", "Lasalle","Lambdin", "Bishop P L Scott"),
-  dc_incorrect = c("Kingshighway Memorial", "Martin Luther King", "Mlk", "Red Fox",
+  incorrect = c("Kingshighway Memorial", "Martin Luther King", "Mlk", "Red Fox",
                    "Samuel T Shepard", "Samuel Shepard", "Sam Shepard", "Mt Pleasant",
-                   "Morganford", "La Salle", "Lamblin", "Bishop Scott"),
-  stringsAsFactors = FALSE
+                   "Morganford", "La Salle", "Lamblin", "Bishop Scott")
 )
 
-stdStreets <- dplyr::bind_rows(dataCorr1, dataCorr2)
-stdStreets <- dplyr::bind_rows(stdStreets, dataCorr3)
-stdStreets <- dplyr::arrange(stdStreets, dc_correct)
+stl_std_streets <- dplyr::arrange(stl_std_streets, correct)
 
 # save as .RData file for inclusion in stldata package
 
-save(stdStreets, file = "data/stdSteets.RData")
+usethis::use_data(stl_std_streets, overwrite = TRUE)
